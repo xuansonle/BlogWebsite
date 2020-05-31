3931cc7f9ff8a776b879d7fd2dc04eeb4c79e756
Xuan Son Le <lexuanson1992@gmail.com>
2020-05-31 20:08:17 +0200

    upload avatar added

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app.models import User 
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
@@ -23,8 +25,26 @@ class RegistrationForm(FlaskForm):
             raise ValidationError("That email is taken. Please choose another one")
     
 class LoginForm(FlaskForm):
-    # user =  StringField("User/Email", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    email =  StringField("Username/Email", validators=[DataRequired()])
+    # email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember me")
     submit = SubmitField("Login")
+    
+    
+class UpdateAccountForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    picture = FileField("Update profile picture", validators=[FileAllowed(["jpg","jpeg","png"])])
+    submit = SubmitField("Update")
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("That username is taken. Please choose another one")
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError("That email is taken. Please choose another one")
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
-from flask import render_template, url_for, flash, redirect, request
 from app import app, db, bcrypt
-from app.forms import RegistrationForm, LoginForm
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from app.models import User, Post
-from flask_login import login_user, current_user, logout_user, login_required # User Authentication
-    
+from flask import render_template, url_for, flash, redirect, request
+import secrets, os
+# User Authentication
+from flask_login import login_user, current_user, logout_user, login_required
+from PIL import Image
+
 posts = [
     {
         'author': 'Corey Schafer',
@@ -19,22 +22,26 @@ posts = [
     }
 ]
 
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html", posts=posts)
 
+
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
 
-@app.route("/register", methods=["GET","POST"])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if current_user.is_authenticated: #check if user already logged in -> back to homepage
+    if current_user.is_authenticated:  # check if user already logged in -> back to homepage
         redirect(url_for("home"))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode("utf-8")
         user = User(username=form.username.data,
                     email=form.email.data,
                     password=hashed_password)
@@ -44,18 +51,21 @@ def register():
         return redirect(url_for("login"))
     return render_template("register.html", title="Register", form=form)
 
-@app.route("/login", methods=["GET","POST"])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if current_user.is_authenticated: #check if user already logged in -> back to homepage
+    if current_user.is_authenticated:  # check if user already logged in -> back to homepage
         redirect(url_for("home"))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+        user = User.query.filter((User.email == form.email.data) | (
+            User.username == form.email.data)).first()
         # User Authentication for login: check if user exists and password is correct
         if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data) #do the log in
-            next_page = request.args.get("next") #get the next parameter from the url (this is where we were trying to go to before the login_required blocked us)
-            return redirect(next_page) if next_page else redirect(url_for("home")) 
+            login_user(user, remember=form.remember.data)  # do the log in
+            # get the next parameter from the url (this is where we were trying to go to before the login_required blocked us)
+            next_page = request.args.get("next")
+            return redirect(next_page) if next_page else redirect(url_for("home"))
         else:
             flash("Login failed! Please check username and password", "danger")
     return render_template("login.html", title="Login", form=form)
@@ -64,9 +74,39 @@ def login():
 @app.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for("home"))
+    return redirect(url_for("login"))
 
-@app.route("/account")
-@login_required #force user to login before they can see this page -> need to define where the login route is located (see init.py)
+def save_picture(upload_picture):
+    #randomize the name of the image, make sure that we use the same extension 
+    random_hex=secrets.token_hex(8)
+    _, f_ext = os.path.splitext(upload_picture.filename)
+    picture_filename = random_hex + f_ext
+    picture_path = os.path.join(app.root_path,"static/profile_pics",picture_filename)
+    #resize then save the picture in our server
+    output_size = (125,125)
+    im = Image.open(upload_picture)
+    im.thumbnail(output_size)
+    im.save(picture_path)
+    
+    return picture_filename
+
+@app.route("/account", methods=["GET", "POST"])
+# force user to login before they can see this page -> need to define where the login route is located (see init.py)
+@login_required
 def account():
-    return render_template("account.html", title="Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_filename = save_picture(form.picture.data)
+            current_user.image_file=picture_filename
+        current_user.username=form.username.data
+        current_user.email=form.email.data
+        db.session.commit()
+        flash("Your account has been updated!","success")
+        return redirect(url_for("account")) #->force browser to send a get request
+    elif request.method=="GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for(
+        "static", filename=f"profile_pics/{current_user.image_file}")
+    return render_template("account.html", title="Account", image_file=image_file, form=form)
